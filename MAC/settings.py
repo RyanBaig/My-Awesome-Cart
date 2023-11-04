01400e3fdfab5984cbe82e39a2ce39d612bc2088
RyanGamingYT <rg373429@gmail.com>
2023-11-04 22:01:47 +0500

    Hi

--- MAC/settings.py
+++ MAC/settings.py
@@ -76,15 +76,12 @@ WSGI_APPLICATION = "MAC.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django-db',
-        'USER': 'default',
-        'PASSWORD': '9KADaUm1BgJG',
-        'HOST': 'ep-round-mouse-89961928-pooler.us-east-1.postgres.vercel-storage.com',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
