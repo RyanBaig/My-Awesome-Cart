fe033689aa01e6018b584ade834f58522b3ee9b7
RyanGamingYT <rg373429@gmail.com>
2023-11-04 21:44:15 +0500

    lets see if this workss

--- MAC/settings.py
+++ MAC/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = "MAC.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-db',
+        'USER': 'default',
+        'PASSWORD': '9KADaUm1BgJG',
+        'HOST': 'ep-round-mouse-89961928-pooler.us-east-1.postgres.vercel-storage.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
