f56ea357275eb7c47e779b37d5004308027aca3d
RyanGamingYT <rg373429@gmail.com>
2023-11-05 15:19:57 +0500

    commit 1

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     product_id = models.AutoField
-    product_name = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=26)
     category = models.CharField(max_length=50, default="")
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
