d83dc1948480d477c83017ab44317c17888ff268
RyanGamingYT <rg373429@gmail.com>
2023-11-10 01:59:57 +0500

    .

--- MAC/urls.py
+++ MAC/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+# from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
-    path('', views.index, name="Home"),
+    # path('', views.index, name="Home"),
+    path('', include('shop.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
