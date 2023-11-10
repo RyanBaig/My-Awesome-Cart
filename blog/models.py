7b5a431cef20a51d6be5a39b0c0ea058d70a28f5
RyanGamingYT <rg373429@gmail.com>
2023-11-10 05:57:14 +0500

    made some blog posts and displayed them

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    head0 = models.CharField(default='', max_length=500)
+    chead0 = models.CharField(default='', max_length=5000)
+    head1 = models.CharField(default='', max_length=500)
+    chead1 = models.CharField(default='', max_length=5000)
+    head2 = models.CharField(default='', max_length=500)
+    chead2 = models.CharField(default='', max_length=5000)
+    pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to="blog/images", default="")
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blogpost
 
 # Create your views here.
-def index(request):
-    return render(request, "blog/index.html")
+# def index(request):
+#     return render(request, "blog/index.html")
 
 def blogpost(request):
     return render(request, "blog/blogpost.html")
+
+#                      EXERCISE
+def index(request):
+    allPosts = []
+    posts = Blogpost.objects.values('title', 'chead0', 'pub_date', 'thumbnail', 'post_id')
+    # put all the different posts in a list
+    allPosts.append(posts)
+    print(allPosts)
+    return render(request, 'blog/index.html', {'posts': allPosts[0]})
+    
