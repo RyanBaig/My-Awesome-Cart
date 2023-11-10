b28ab761f0c951582415c22e4753a98dd5aa7df6
RyanGamingYT <rg373429@gmail.com>
2023-11-10 05:13:21 +0500

    onwards to the blog section!

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
+
+def blogpost(request):
+    return render(request, "blog/blogpost.html")
